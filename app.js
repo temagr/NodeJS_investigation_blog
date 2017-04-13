dc85b81498c351878a75dd5c48a7548ebaf2f8bc
e.rotar <e.rotar@itransition.com>
2017-04-13 11:18:59 +0300

    add session support into application (draft version)

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    dataBase = require('./database')('Blog', 'admin', '123456', {
-        host: 'RUKAVITSINI',
-        port: 1543,
-        dialect: 'mssql'
-    });
+    cookieParser = require('cookie-parser')
+    var session = require('express-session');
+    // dataBase = require('./database')('Blog', 'admin', '123456', {
+    //     host: 'RUKAVITSINI',
+    //     port: 1543,
+    //     dialect: 'mssql'
+    // });
 
 let app = express();
 
@@ -19,21 +20,39 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true, cookie: {} }));
 
 app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 passport.use('local', new LocalStrategy((username, password, done) => {
-    dataBase.query(`SELECT userID,username, password FROM Credentials WHERE username='${username}' and password='${password}'`).spread((result, metadata) => {
-        console.log(result);
-        if (result.length === 1) {
-            return done(null, {
-                id: result.userId,
-                username: result.username
-            });
-        } else {
-            return done(null, false);
-        }
-    });
+    if(username == "admin" && password == "password") {
+        return done(null, {
+            id: 1,
+            username: username
+        });
+    } else {
+        return(null, false);
+    }
+    // dataBase.query(`SELECT userID,username, password FROM Credentials WHERE username='${username}' and password='${password}'`).spread((result, metadata) => {
+    //     console.log(result);
+    //     if (result.length === 1) {
+    //         return done(null, {
+    //             id: result.userId,
+    //             username: result.username
+    //         });
+    //     } else {
+    //         return done(null, false);
+    //     }
+    // });
 }))
 
 app.get('/', (req, res) => {
@@ -51,7 +70,18 @@ app.post('/submitted', passport.authenticate('local', {
 }));
 
 app.get('/success', (req, res) => {
-    res.send("Your authentication was successfull");
+    var session = req.session;
+    if (session.views) {
+        session.views++
+        res.write('<p>views: ' + session.views + '</p>')
+        res.write('<p>expires in: ' + (session.cookie.maxAge / 1000) + 's</p>')
+        res.end()
+    } else {
+        session.views = 1
+        res.write('<p> username:' + req.session.passport.user.username + '</p>');
+        res.end('welcome to the session demo. refresh!')
+    }
+    console.log(req.session);
 });
 
 app.get('/failure', (req, res) => {

11d027d60354e36b0f935a16124220454675960a
Ilya Rucavitcyn <i.rukavitsin@itransition.com>
2017-04-14 16:41:34 +0300

    Simple logout implemented

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy,
     FacebookStrategy = require('passport-facebook').Strategy,
-    User = require('../models/user.model');
+    User = require('../models/user.model'),
+    configAuth = require('./auth');
 
 
 module.exports = function(passport){
@@ -26,4 +27,34 @@ module.exports = function(passport){
             }
           });
   }));
+
+  // passport.use(new FacebookStrategy({
+  //       clientID        : configAuth.facebookAuth.clientID,
+  //       clientSecret    : configAuth.facebookAuth.clientSecret,
+  //       callbackURL     : configAuth.facebookAuth.callbackURL
+  //   },
+  //   (token, refreshToken, profile, done) => {
+  //       process.nextTick(() => {
+  //           User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+  //               if (err)
+  //                   return done(err);
+  //               if (user) {
+  //                   return done(null, user);
+  //               } else {
+  //                   var newUser            = new User();
+  //                   newUser.facebook.id    = profile.id; // set the users facebook id
+  //                   newUser.facebook.token = token; // we will save the token that facebook provides to the user
+  //                   newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+  //                   newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+  //                   newUser.save(function(err) {
+  //                       if (err)
+  //                           throw err;
+  //                       return done(null, newUser);
+  //                   });
+  //               }
+  //           });
+  //       });
+  //
+  //   }));
+
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,30 @@
 const express = require('express'),
     router = express.Router();
 
-module.exports = function(passport){
+module.exports = function(passport) {
 
-  router.get('/', (req, res) => {
-      res.render('index');
-  });
+    router.get('/', (req, res) => {
+        res.render('index');
+    });
 
-  router.get('/login', (req, res) => {
-      res.render('login');
-  })
+    router.get('/login', (req, res) => {
+        res.render('login');
+    })
 
-  router.get('/profile', (req, res) => {
-      res.render('profile');
-  });
+    router.get('/profile', (req, res) => {
+        res.render('profile');
+    });
 
-  router.post('/login', passport.authenticate('local', {
-      session: true,
-      successRedirect: '/profile',
-      failureRedirect: '/'
-  }));
+    router.post('/login', passport.authenticate('local', {
+        session: true,
+        successRedirect: '/profile',
+        failureRedirect: '/'
+    }));
 
-  return router;
+    router.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    return router;
 }

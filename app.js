b338692ee2acb40b9d03da3252317528ef68b943
Ilya Rucavitcyn <i.rukavitsin@itransition.com>
2017-04-29 13:17:36 +0300

    Logger implemented

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const routes = require('./routes/routes')(passport),
     configAuth = require('./config/auth'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
     session = require('express-session');
 
 const app = express();
@@ -18,17 +19,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // App middleware
+
+if(process.env.NODE_ENV === "development"){
+  app.use(morgan('dev'));
+}
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: true, cookie: {}}));
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use('/', routes)
-// define if we want run unit tests by URL with ?test=1
-app.use(function(req, res, next) {
-    res.locals.runUnitTests = (req.query.test === '1');
-    next();
-});
 
 module.exports = app;

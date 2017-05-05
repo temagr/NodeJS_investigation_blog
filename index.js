079d0508dd8686e6de1c5292086a3906a48b5abd
Ilya Rucavitcyn <ilya.rucavitcyngmail.com>
2017-05-05 13:01:13 +0300

    Keys added and https is established

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const app = require('./app');
+const app = require('./app'),
+    https = require('https'),
+    fs = require('fs');
 
 app.listen(app.get('port'), () => {
     console.log('Node app is running on port', app.get('port'));
 });
+
+const httpsOptions = {
+    key: fs.readFileSync('./ssl/private.key'),
+    cert: fs.readFileSync('./ssl/certificate.pem')
+};
+
+const httpsServer = https
+    .createServer(httpsOptions, app)
+    .listen(3443);
